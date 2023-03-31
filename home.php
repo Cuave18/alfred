41557173d136b2ecc737963d62c5b718067bafb5
Alfredo Cuave <113578525+Cuave18@users.noreply.github.com>
2023-03-31 20:32:00 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,32 @@
+<?php
+include_once("header.php");
+?>
+
+  
+<div class="post-item clearfix">
+                  
+<h4><a href="#">Id quia et et ut maxime similique occaecati ut</a></h4>
+<p>Fugiat voluptas vero eaque accusantium eos. Consequuntur sed ipsam et totam...</p>
+</div>
+
+<div class="post-item clearfix">
+
+
+    </div>
+
+<div class="post-item clearfix">
+      
+
+ 
+                
+                 
+                </div>
+
+              </div><!-- End sidebar recent posts-->
+
+            </div>
+          </div><
+          <h1>Bem Vindo</h1>  
+ <?php 
+ include_once("footer.php");
+ ?>
